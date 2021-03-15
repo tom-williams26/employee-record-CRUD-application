5f4cecff9c46fd8edcdf5df5125f4b9f1f2322d7
Tom Williams <tom.williams0593@outlook.com>
2021-03-15 18:29:14 +0000

    user interface design

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
 
 function App() {
-  return <div className="App">hello all</div>;
+  return (
+    <div className="App">
+      <div>
+        <h1>Employee Record System</h1>
+      </div>
+      <div className="employee-entry">
+        <div className="record">
+          <label>Employee name:</label>
+          <input type="text" />
+        </div>
+        <div className="record">
+          <label>Employee age:</label>
+          <input type="number" />
+        </div>
+        <div className="record">
+          <label>Employee address:</label>
+          <input type="text" />
+        </div>
+        <div className="record">
+          <label>Employee position:</label>
+          <input type="text" />
+        </div>
+        <div className="record">
+          <label>Employee wage (per annum) £:</label>
+          <input type="number" />
+        </div>
+        <button>Add Employee</button>
+        <button>Update Employee</button>
+        <button>Delete Employee</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
